a54ffaf79be09da77f56f77a20e8b76727fa615e
syaimahnisa <140956114+syaimahnisa@users.noreply.github.com>
2023-12-04 12:04:42 +0700

    Add files via upload

---
+++ belanja.js
@@ -0,0 +1,10 @@
+let belanja = perseInt(prompt("masukan jumlah belanja"))
+switch (true){
+    case balanja < 100000:
+        document.write(`total belanja = ${belanja}`);
+        break;
+    case belanja >= 100000:
+        document.write(`total belanja = ${belanja}`);
+        break
+    default.writeInt(`kamu salah masukan nilai`)
+}
---
+++ main.js
@@ -0,0 +1,42 @@
+let pilihan =prompt("pilih bulan")
+switch (pilihan) {
+    case 'januari':
+        document.write(`ini bulan ${pilihan}`);
+        break;
+    case 'februari':
+        document.write(`ini bulan ${pilihan}`);
+        break;
+    case 'maret':
+        document.write(`ini bulan ${pilihan}`);
+        break;
+    case 'april':
+        document.write(`ini bulan ${pilihan}`);
+        break;
+    case 'mei':
+        document.write(`ini bulan ${pilihan}`);
+        break;
+    case'juni':
+        document.write(`ini bulan ${pilihan}`);
+        break;
+    case 'juli':
+        document.write(`ini bulan ${pilihan}`);
+        break;
+    case 'agustus':
+        document.write(`ini bulan ${pilihan}`);
+        break;
+    case 'september':
+        document.write(`ini bulan ${pilihan}`);
+        break;
+    case'oktober':
+        document.write(`ini bulan ${pilihan}`);
+        break;
+    case'november':
+        document.write(`ini bulan ${pilihan}`);
+        break;
+    case'desember':
+        document.write(`ini bulan ${pilihan}`);
+        break;
+    default:
+        document.write(`ini bukan bulan`)
+        // code
+}
